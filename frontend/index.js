3bca1a53f7a092d553468e863ae1eab6499b4fbb
Talib Khan <talibkhan2407@gmail.com>
2026-08-13 16:46:22 +0530

    toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,28 @@
-alert("Welcome to bootcamp")
+//alert("Welcome to bootcamp")
+var user=[
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "image": "john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "image": "jane.png"
+    }
+];
+
+var curId = 0;
+function toggleUser(){
+    // alert("Came here");
+    curId = (curId + 1) % user.length;
+    var userName = document.getElementById("user-name");
+    var userGender = document.getElementById("user-gender");
+    var userImage = document.getElementById("user-image");
+
+
+    userName.innerHTML = user[curId].name;
+    userGender.innerHTML = user[curId].gender;
+    userImage.src = user[curId].image;
+
+}

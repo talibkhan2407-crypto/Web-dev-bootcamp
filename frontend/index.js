e461c03b9dc04e2c741cf41f13a523449aab64dd
Talib Khan <talibkhan2407@gmail.com>
2026-08-14 11:57:15 +0530

    links

--- frontend/index.js
+++ frontend/index.js
@@ -26,3 +26,25 @@ function toggleUser(){
     userImage.src = user[curId].image;
 
 }
+function randomUser() {
+    fetch("https://randomuser.me/api")
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      var userName = document.getElementById("user-name");
+      var userGender = document.getElementById("user-gender");
+      var userImage = document.getElementById("user-image");
+
+      var newUserName = data.results[0].name.first + " " + data.results[0].name.last;
+      var newUserGender = data.results[0].gender;
+      var newUserImage = data.results[0].picture.large;
+      userName.innerHTML = newUserName;
+      userGender.innerHTML = newUserGender;
+      userImage.src = newUserImage;
+
+    })
+    .catch(function(err) {
+      console.log("Error occured : " + err);
+    })
+}
